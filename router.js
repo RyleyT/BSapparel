879c037cbe70af81ad33cdcd9c818d14657c0993
Luke Williams <law67@students.uwf.edu>
2020-09-10 17:49:14 -0500

    routes set up in server.js

--- router.js
+++ router.js
@@ -1,37 +1,52 @@
-const httpStatus = require("http-status-codes"),
-htmlContentType = {
-    "Content-Type": "text/html"
-},
+var express = require('express');
+var router = express.Router();
 
-routes = { //Here we define a routes object to store routes mapped to POST and GET requests.
-"GET": { //Our get requests stored here
-    "/info": (req, res) => {
-        res.writeHead(httpStatus.OK, {
-            "Content-Type":"text/plain"
-        })
-        res.end("Welcome to the Info Page!")
-    }
-},
-'POST': {} //Our post requests stored here
-};
+// Home page
+router.get('/', function (req, res) {
+  res.render('index');
+})
 
-exports.handle = (req, res) => { //Create a handle function to process route and callback functions
-    try {
-        if(routes[req.method][req.url]) {
-            routes[req.method][req.url](req,res);
-        } else {
-            res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
-            res.end("<h1>File not found</h1>")
-        }
-    } catch (ex) {
-        console.log("Error: " + ex);
-    }
-};
+// Info page
+router.get('/info', function (req, res) {
+  res.render('info');
+})
 
-exports.get = (url, action) => { //Build get and post functions to register routes from main.js
-    routes["GET"][url] = action;
-};
+module.exports = router;
 
-exports.post = (url, action) => {
-    routes["POST"][url] = action;
-};
+// const httpStatus = require("http-status-codes"),
+// htmlContentType = {
+//     "Content-Type": "text/html"
+// },
+
+// routes = { //Here we define a routes object to store routes mapped to POST and GET requests.
+// "GET": { //Our get requests stored here
+//     "/info": (req, res) => {
+//         res.writeHead(httpStatus.OK, {
+//             "Content-Type":"text/plain"
+//         })
+//         res.end("Welcome to the Info Page!")
+//     }
+// },
+// 'POST': {} //Our post requests stored here
+// };
+
+// exports.handle = (req, res) => { //Create a handle function to process route and callback functions
+//     try {
+//         if(routes[req.method][req.url]) {
+//             routes[req.method][req.url](req,res);
+//         } else {
+//             res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
+//             res.end("<h1>File not found</h1>")
+//         }
+//     } catch (ex) {
+//         console.log("Error: " + ex);
+//     }
+// };
+
+// exports.get = (url, action) => { //Build get and post functions to register routes from main.js
+//     routes["GET"][url] = action;
+// };
+
+// exports.post = (url, action) => {
+//     routes["POST"][url] = action;
+// };
--- server.js
+++ server.js
@@ -1,56 +1,85 @@
-const port = 3000,
-http = require("http"),
-httpStatus = require("http-status-codes"),
-router = require("./router")
-fs = require("fs");
+const express = require('express');
+const path = require('path');
+// const router = require("./router");
 
-plainTextContentType = {
-    "Content-Type": "text/plain"
-},
-htmlContentType = {
-    "Content-Type": "text/html"
-},
-cssContentType = {
-    "Content-Type": "text/css"
-},
-jsContentType = {
-    "Content-Type": "text/javascript"
-}
-jpgContentType = {
-    "Content-Type": "image/jpg"
-},
-pngContentType = {
-    "Content-Type": "image/png"
-},
+var app = express();
 
+app.set('views', (path.join(__dirname, 'views')));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
 
-customReadFile = (file, res) => { //A custom read file to reduce repetition
-    fs.readFile(`./${file}`, (errors, data) => {
-        if(errors) {
-            console.log("Error reading the file...");
-        }
-        res.end(data);
-    });
-};
+// app.use(router);
 
-router.get("/", (req, res) => {
-    res.writeHead(httpStatus.OK, htmlContentType);
-    customReadFile("views/index.html", res);
-});
-
-router.get("/index.html", (req, res) => {
-    res.writeHead(httpStatus.OK, htmlContentType);
-    customReadFile("views/index.html", res);
-});
-
-router.get("/sampleImage.jpg", (req, res) => {
-    res.writeHead(httpStatus.OK, )
+// Home page
+app.get('/', function (req, res) {
+    res.render('index.html');
 })
 
-router.post("/", (req, res) => {
-    res.writeHead(httpStatus.OK, plainTextContentType);
-    res.end("POSTED");
+// Info page
+app.get('/info', function (req, res) {
+    res.render('info.html');
+})
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.');
 });
 
-http.createServer(router.handle).listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+// const port = 3000,
+// http = require("http"),
+// httpStatus = require("http-status-codes"),
+// router = require("./router")
+// fs = require("fs");
+
+// plainTextContentType = {
+//     "Content-Type": "text/plain"
+// },
+// htmlContentType = {
+//     "Content-Type": "text/html"
+// },
+// cssContentType = {
+//     "Content-Type": "text/css"
+// },
+// jsContentType = {
+//     "Content-Type": "text/javascript"
+// }
+// jpgContentType = {
+//     "Content-Type": "image/jpg"
+// },
+// pngContentType = {
+//     "Content-Type": "image/png"
+// },
+
+
+// customReadFile = (file, res) => { //A custom read file to reduce repetition
+//     fs.readFile(`./${file}`, (errors, data) => {
+//         if(errors) {
+//             console.log("Error reading the file...");
+//         }
+//         res.end(data);
+//     });
+// };
+
+// router.get("/", (req, res) => {
+//     res.writeHead(httpStatus.OK, htmlContentType);
+//     customReadFile("views/index.html", res);
+// });
+
+// router.get("/index.html", (req, res) => {
+//     res.writeHead(httpStatus.OK, htmlContentType);
+//     customReadFile("views/index.html", res);
+// });
+
+// router.get("/sampleImage.jpg", (req, res) => {
+//     res.writeHead(httpStatus.OK, )
+// })
+
+// router.post("/", (req, res) => {
+//     res.writeHead(httpStatus.OK, plainTextContentType);
+//     res.end("POSTED");
+// });
+
+// http.createServer(router.handle).listen(port);
+// console.log(`The server has started and is listening on port number: ${port}`);
