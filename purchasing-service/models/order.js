6d2c2835246506aef49c3c8a770e126974ef1847
Luke Williams <law67@students.uwf.edu>
2020-11-19 07:37:21 -0800

    updated order schema with date

--- purchasing-service/models/order.js
+++ purchasing-service/models/order.js
@@ -1,14 +1,14 @@
 const { Schema, model } = require("mongoose");
 
 const OrderSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'User'},
-    items: [{type: Schema.Types.ObjectId, ref: 'Item'}],
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    items: [{type: Schema.Types.ObjectId, ref: 'Item', required: true}],
     total: {
         required: true,
         trim: true,
         type: Number
     },
-    date: Date.now()
+    date: {type: Date, default: Date.now}
 });
 
 const Order = model("Order", OrderSchema);
