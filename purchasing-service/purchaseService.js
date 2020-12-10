487eadbaaba9f4049c62e323f6e4d73a162f675d
Luke Williams <law67@students.uwf.edu>
2020-12-10 14:38:28 -0800

    logging for purchasing service complete

--- purchasing-service/purchaseService.js
+++ purchasing-service/purchaseService.js
@@ -1,8 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var morgan = require('morgan');
-const PORT = process.env.PORT || 7000;
-const mongoose = require('mongoose');
+const express = require('express'),
+  createError = require('http-errors'),
+  morgan = require('morgan'),
+  PORT = process.env.PORT || 7000,
+  mongoose = require('mongoose'),
+  appRequestId = require('express-request-id')(),
+  logger = require('./logController');
 
 // Setting up proxy server to listen for api-gateway.
 var http = require('http');
@@ -17,9 +19,9 @@ server = http.createServer(function (req, res) {
 const Order = require("./models/order");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://ryleyt:1qaz@cluster0.h0wyn.mongodb.net/<dbname>?retryWrites=true&w=majority", {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
 var app = express();
@@ -27,72 +29,157 @@ var app = express();
 app.use(morgan('Purchase-Service\: :method :url :status :res[content-length] - :response-time ms'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(appRequestId);
 
 // receive all orders from db
-app.get("/api/orders", (req, res) => {
-  Order.find()
-  .then((orders) => {
-    // res.setHeader("ID", Math.random() * (1000000 - 100) + 100);
-
-    // Log response with log-service
-    let log = {
-      service: "Purchase",
-      route: "/api/orders",
-      // reponseId: res.ID,
-      message: "returning all orders in db.",
-      date: Date.now
-    }
-    console.log(log);
-    // Send to log-service
-    // fetch(logUrl, log);
-
-    res.json(orders);
-  })
-  .catch((err) => {
-    res.json(err.message);
-  })
-});
-
-// create new order history in db
-app.post("/api/orders", function (req, res) {
-  // console.log(req.body);
-  Order.create(req.body)
-      .then((order) => {
-        // res.setHeader("ID", Math.random() * (1000000 - 100) + 100);
-
+app
+  .get("/api/orders", (req, res) => {
+    Order.find()
+      .then((orders) => {
         // Log response with log-service
         let log = {
           service: "Purchase",
           route: "/api/orders",
-          // reponseId: res.ID,
-          message: "creating order in db.",
+          responseId: res.getHeader(`x-request-id`),
+          message: "responding with all orders",
           date: Date.now
         }
-        console.log(log);
-        // Send to log-service
-        // fetch(logUrl, log);
-    
+        // Send log to log controller
+        logger.logResponse(log);
+
+        res.json(orders);
+      })
+      .catch((err) => {
+        // Log response with log-service
+        let log = {
+          service: "Purchase",
+          route: "/api/orders",
+          responseId: res.getHeader(`x-request-id`),
+          message: "responding with error: unable to get orders",
+          date: Date.now
+        }
+        // Send log to log controller
+        logger.logResponse(log);
+        res.json(err.message);
+      })
+  })
+  // create new order history in db
+  .post("/api/orders", function (req, res) {
+    // console.log(req.body);
+    Order.create(req.body)
+      .then((order) => {
+        // Log response with log-service
+        let log = {
+          service: "Purchase",
+          route: "/api/orders",
+          responseId: res.getHeader(`x-request-id`),
+          message: "creating a new order",
+          date: Date.now
+        }
+        // Send log to log controller
+        logger.logResponse(log);
+
         res.json(order);
       })
       .catch((err) => {
-          res.json(err.message);
+        // Log response with log-service
+        let log = {
+          service: "Purchase",
+          route: "/api/orders",
+          responseId: res.getHeader(`x-request-id`),
+          message: "responding with error: unable to create order",
+          date: Date.now
+        }
+        // Send log to log controller
+        logger.logResponse(log);
+        res.json(err.message);
       });
-});
+  });
+
+  app
+    .get('/api/orders/:id', function(req, res) {
+      //get orders with the specified id
+      Order.find({ $or:[{user: req.params.id},{_id: req.params.id}] })
+      .then(orders => {
+        // Log response with log-service
+        let log = {
+          service: "Purchase",
+          route: "/api/orders/:id",
+          responseId: res.getHeader(`x-request-id`),
+          message: "responding with orders by user or order id provided",
+          date: Date.now
+        }
+        // Send log to log controller
+        logger.logResponse(log);
+        res.json(orders);
+      }).catch(error => {
+        // Log response with log-service
+        let log = {
+          service: "Purchase",
+          route: "/api/orders/:id",
+          responseId: res.getHeader(`x-request-id`),
+          message: "responding with error: unable to get specified orders",
+          date: Date.now
+        }
+        // Send log to log controller
+        logger.logResponse(log);
+        res.json(error);
+      })
+    })
+    .put('/api/orders/:id', function(req,res) {
+      //update the order specified by the id
+      Order.findByIdAndUpdate(req.params.id, req.body)
+      .then(result => {
+        // Log response with log-service
+        let log = {
+          service: "Purchase",
+          route: "/api/orders/:id",
+          responseId: res.getHeader(`x-request-id`),
+          message: "updated order with the specified id",
+          date: Date.now
+        }
+        // Send log to log controller
+        logger.logResponse(log);
+        res.json(result);
+      }).catch(error => {
+        // Log response with log-service
+        let log = {
+          service: "Purchase",
+          route: "/api/orders",
+          responseId: res.getHeader(`x-request-id`),
+          message: "responding with error: unable to get specified order for update",
+          date: Date.now
+        }
+        // Send log to log controller
+        logger.logResponse(log);
+        res.json(error);
+      })
+    })
+    // .delete('api/orders/:id', function(req,res) {
+    //   Order.findByIdAndDelete(req.params.id)
+    //   .then(result => {
+    //     res.json({
+    //       message: "order with id " + req.params.id + " deleted"
+    //     }).catch(error => {
+    //       res.json(error);
+    //     })
+    //   })
+    // })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
 app.listen(PORT, () => {
